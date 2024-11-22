9d9ea85fb7ab2a0e4e8ced39fb53db77e0748a73
CharlesMich <charleedongo@hotmail.com>
2024-11-21 23:38:22 -0600

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Charles Michael on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
