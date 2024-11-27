402bb75f2b71550f5f47ba05e9c9bce96a2a6ca9
CharlesMich <charleedongo@hotmail.com>
2024-11-27 13:22:53 -0600

    nov 27

--- BucketList/ContentView4.swift
+++ BucketList/ContentView4.swift
@@ -9,11 +9,11 @@ import SwiftUI
 import MapKit
 
 struct ContentView4: View {
-    
+    @State private var position = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5077, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)))
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        Map()
+       
+        Map(position: $position)
 //            .mapStyle(.imagery)
             .mapStyle(.hybrid)
             .mapStyle(.hybrid(elevation: .realistic))
